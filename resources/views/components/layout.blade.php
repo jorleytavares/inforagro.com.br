93c18f586e875abf9cf576ffaea6b72eb7ef5390
jorleytavares <tavaresjorley@gmail.com>
2026-01-17 01:25:54 -0400

    Configura favicon responsivo no layout público

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,15 @@
     <meta name="description" content="{{ $description ?? 'Notícias, cotações e tecnologia para o agronegócio brasileiro.' }}">
     <meta name="theme-color" content="#166534">
 
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/images/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon-32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon-16.png') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('assets/images/favicon.svg') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ asset('assets/images/site.webmanifest') }}">
+    <meta name="apple-mobile-web-app-title" content="InforAgro">
+
     @if(isset($canonical))
     <link rel="canonical" href="{{ $canonical }}">
     @endif
